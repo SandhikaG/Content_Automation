8354ef14634e4d7a9cdd8f45cc0b398ec64d8a48
sandhika <sandhika.g@msds.christuniversity.in>
2026-04-07 17:11:06 +0530

    Update deployed backend url

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import requests
 
-BACKEND_URL = "http://127.0.0.1:8000"
+BACKEND_URL = "https://content-automation-1.onrender.com"
 
 st.set_page_config(page_title="AI Content Generator", layout="wide")
 
